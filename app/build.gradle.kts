d4386291956f3d784726a2f635a863bb0cd273b8
Rabin Hood <valid.zarei.2afm@gmail.com>
2026-09-26 05:37:01 +0330

    bump version to 1.0.1 for first official signed release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 2
-        versionName = "1.0.0"
+        versionName = "1.0.1"
     }
 
     // یک APK جداگانه برای هر پردازنده + نسخهٔ universal برای همهٔ دستگاه‌ها
